026b80c4ee408fa55df48b0005b56050736cac5f
David Abenhaim <68952247+ZiteiX@users.noreply.github.com>
2021-12-02 12:43:01 +0200

    new changes

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,6 @@ import { getFeaturedPosts, getAllPosts } from "../lib/posts-util";
 function HomePage({ allPosts, featuresPosts }) {
   return (
     <>
-      <Head>
-        <title>Javascript Blog - From Zero to Hero</title>
-        <meta name="description" content="בלוג בעברית על ג׳אוה סקריפט" />
-        <meta property="og:title" content="Javascript - From Zero to Hero" />
-        <meta
-          property="og:url"
-          content="https://my-blog-next-js-delta.vercel.app/"
-        />
-        <meta property="og:description" content="Javascript Blog" />
-        <meta property="og:image" content="/images/siteicon" />
-        <link rel="icon" herf="/favicon.ico" />
-      </Head>
       <Hero />
       <FeaturedPosts posts={featuresPosts} />
       <AllPosts posts={allPosts} />
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -25,7 +25,7 @@ export function getStaticProps(context) {
       post: curPost,
       nextPostSlug: nextPostSlug,
     },
-    revalidate: 600,
+    revalidate: 6000,
   };
 }
 
