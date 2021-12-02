e6ee0e53cf0b639eeeb21971eeb56ce29688322f
David Abenhaim <68952247+ZiteiX@users.noreply.github.com>
2021-12-02 12:55:15 +0200

    fixed - alt for images, bug in the meta

--- pages/_app.js
+++ pages/_app.js
@@ -11,20 +11,6 @@ function MyApp({ Component, pageProps }) {
         <title>Javascript Blog - From Zero to Hero</title>
         <meta property="og:description" content="Javascript Blog" />
         <meta property="og:title" content="Javascript - From Zero to Hero" />
-        <Image
-          src="http://euro-travel-example.com/thumbnail.jpg"
-          height="500px"
-          width="500px"
-        />
-        <meta
-          property="og:image"
-          content="http://euro-travel-example.com/thumbnail.jpg"
-        />
-        <meta
-          property="og:url"
-          content="https://my-blog-next-js-6frghu2mw-ziteix.vercel.app"
-        />
-        {/* <meta property="og:image" content="/images/siteicon" /> */}
         <link rel="icon" herf="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
